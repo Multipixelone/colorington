f2af542ae5ba1c93a1700c5daa5d24c00745159b
Finn <finn@cnwr.net>
2023-09-15 11:14:37 -0400

    Cleanup

--- example.py
+++ example.py
@@ -2,24 +2,19 @@
 from plexapi.server import PlexServer
 from plexapi.utils import download
 from variables import baseurl, token
-#from colorthief import ColorThief
 import time
-import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
 
 plex = PlexServer(baseurl, token)
 
-for media in plex.sessions():
-    print(media.viewOffset)
-    print(media.duration)
-
-for media in plex.sessions():
-    if media.type == "track" and media.usernames[0] == "Tunnelmaker":
-        print(media.title)
-        filename = f"art_{media.parentTitle}.jpg"
-        url = media.parentThumb
-        url = plex.transcodeImage(url, height=350, width=350)
-        download(url, token, filename=filename)
-        print(media.viewOffset)
-        print(media.duration)
+while True:
+    for media in plex.sessions():
+        if media.type == "track" and media.usernames[0] == "Tunnelmaker":
+            filename = f"art_{media.parentTitle}.jpg"
+            print(f"Currently playing: {media.title}")
+            print(f"Album Art: {filename}")
+            if not filename.exists():
+                print(f"Downloading art...")
+                url = media.parentThumb
+                url = plex.transcodeImage(url, height=350, width=350)
+                download(url, token, filename=filename)
+    time.sleep(5)
